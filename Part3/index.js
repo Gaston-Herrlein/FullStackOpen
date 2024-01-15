431bc41ee2cff9a706c6702ac343f55dea12850a
Gaston Herrlein <herrleingaston@gmail.com>
2024-01-15 21:43:41 +0100

    Ejercicio 3.3, 3.4, 3.5 y 3.6

--- Part3/index.js
+++ Part3/index.js
@@ -26,16 +26,62 @@ const notes = [
     }
   ]
 
+const generateId = () => Math.floor(Math.random() * 1000000)
+
+
 app.get("/api/persons", (request, response) => {
     response.json(notes)
 })
 
-app.get("/info", (request, response) => {
+app.get ("/info", (request, response) => {
     const date = new Date()
     const msn = `<p>PhoneBook has info for 2 people</p>
     <p>${date}</p>`
     response.send (msn)
 })
+
+app.get("/api/person/:id", (req, res) => {
+  const id = Number (req.params.id)
+
+  const person = notes.find(person => person.id === id)
+
+  if (person){
+    res.json(person)
+  }
+  else{
+    return res.status(404).json ({error: 'Person not found'}).end()
+  }
+})
+
+app.delete ('/api/person/:id', (req, res) => {
+  const id = Number (req.params.id)
+
+  const persons = notes.filter(person => person.id !== id)
+  res.json(persons) 
+})
+
+app.post('/api/person', (req, res) => {
+  const newId = generateId()
+  const body = req.body
+  if (!body.name || !body.number){
+    return res.status(400).json({error: "Incomplete required fields"})
+  } 
+  else if (notes.map(person => person.name).includes(body.name)) {
+    return res.status(400).json({error: "Name already exists"})
+  }
+  
+  const person = {
+    "name": body.name,
+    "number": body.number,
+    "id": newId
+  }
+
+  const newNotes = notes.concat(person)
+
+  res.json(newNotes)
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log ('Server running in port 3001')
